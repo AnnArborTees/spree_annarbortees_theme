2f2a25ad0c2a7e1823c4411ce2580c0610d601e1
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2014-09-05 14:53:19 -0400

    Initial move of gem from annarbortees-spree

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+branch = '2-2-stable'
+
+gem 'spree', github: 'spree/spree', branch: branch
+gem 'spree_multi_domain', github: 'annarbortees/spree-multi-domain', branch: branch
+
+gemspec
---
+++ app/assets/javascripts/spree/frontend/spree_annarbortees_theme.js
@@ -0,0 +1,8 @@
+// Placeholder manifest file.
+// the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
+
+//= require bootstrap/bootstrap
+//= require holder
+//= require jquery
+//= require jquery-mobile
+//= require_tree .
