283c9bf5d157cabf656098eb3cd8847213784153
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2014-10-08 19:02:15 -0400

    Overriding Authenticity tokens for inputs

--- spec/features/caching/products_spec.rb
+++ spec/features/caching/products_spec.rb
@@ -1,16 +1,15 @@
 require 'spec_helper'
 
-describe 'products', :caching => true do
+describe 'products', caching: true do
+
   let!(:store) { create(:default_store) }
   let!(:store2) { create(:alternative_store) }
-  let!(:product) { create(:product, stores: [store]) }
-  let!(:product2) { create(:product, stores: [store, store2]) }
+  let!(:product) { create(:product, stores: [store, store2]) }
 
 
   describe '#show' do
 
     before do
-      product.update_column(:updated_at, 1.day.ago)
       visit spree.product_path(product)
       expect(cache_writes.count).to eq(1)
       clear_cache_events
@@ -21,17 +20,17 @@ describe 'products', :caching => true do
       expect(cache_writes.count).to eq(0)
     end
 
-    it 'busts the cache for all stores when a product is updated' do
-      product2.touch
-      visit spree.product_path(product2)
+    it 'only caches for the store visited' do
+      visit spree.product_path(product)
 
       # swap stores
       store.update_attribute(:default, 0)
       store2.update_attribute(:default, 1)
-      visit spree.product_path(product2)
+      visit spree.product_path(product)
 
-      expect(cache_writes.count).to eq(2)
+      expect(cache_writes.count).to eq(1)
     end
+
   end
 
 end
