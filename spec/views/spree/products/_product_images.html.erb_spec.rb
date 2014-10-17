6b78d93df946a906dcaac5ae7299979e8421167c
Resonious <metreckk@gmail.com>
2014-10-17 14:42:43 -0400

    (142) all done

--- spec/views/spree/products/_product_images.html.erb_spec.rb
+++ spec/views/spree/products/_product_images.html.erb_spec.rb
@@ -7,8 +7,8 @@ describe 'spree/products/_product_images.html.erb', image_spec: true, story_142:
 
   before(:each) do
     images = [
-      double('Image', id: 0, alt: 'dur'),
-      double('Image', id: 1, alt: 'dur')
+      double('Image', id: 0, alt: 'dur', option_value_id: 5),
+      double('Image', id: 1, alt: 'dur', option_value_id: 7)
     ]
     allow(images.first).to receive_message_chain(:attachment, :url)
       .and_return image_path + "bestshirt.png"
@@ -24,5 +24,11 @@ describe 'spree/products/_product_images.html.erb', image_spec: true, story_142:
     render
     expect(rendered).to have_css 'div.item[data-index="0"]'
     expect(rendered).to have_css 'div.item[data-index="1"]'
+  end
+
+  it 'assigns each image div data-option-value-id of the option_value id of the image' do
+    render
+    expect(rendered).to have_css 'div.item[data-option-value-id="5"]'
+    expect(rendered).to have_css 'div.item[data-option-value-id="7"]'
   end
 end
